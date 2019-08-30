929bcfa27a6514679e74eb932dcbdf6b8aa5c32f
waterjiangjiang <424117560@qq.com>
2019-08-30 22:04:31 +0800

    第一天新增

--- src/js/index.js
+++ src/js/index.js
@@ -8,19 +8,63 @@ $.getJSON('json/bannerslider.json',function(data){
 	}
 	let timer = null;
 	let index = 0;
+	let indexA = 0;
 	autoplay();
+	$('.small').mouseenter(function(){
+		$(this).children('span').css('display','block');
+	})
+	$('.small').mouseleave(function(){
+		$(this).children('span').css('display','none');
+	})
+	$('lt_btn').click(function(){
+		clearInterval(timer);
+		
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	function slide(){
+		
+	}
+	
+	
 	function autoplay(){
+		clearInterval(timer);
 		timer = setInterval(function(){
 			index++;
-			console.log(index);
+			if(index >= len){
+				$('.pic').css('left','0');
+				index = 1;
+			}
+			indexA++;
+			if(indexA >= len - 1){
+				indexA = 0;
+			}
+			
+			// console.log(index);
 			let wid = parseInt($('.pic_box').css('width')) * index;
-			console.log(wid);
+			// console.log(wid);
 			$('.pic').animate({
 				left : - wid
 			},500)
-			if(index === len-1){
-				index = -1;
-			}
+			$('.small ul li').each(function(i){
+				$(this).children('a').css('background','#ccc');
+			})
+			
+			$('.small ul li').eq(indexA).children('a').css('background','#2884c7');	
+			
 		},3000)
 		// timer();
 	}
